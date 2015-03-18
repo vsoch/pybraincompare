f9a149c58f6f581df28cca7ee03062b58208a90e
vsoch <vsochat@stanford.edu>
2015-03-17 18:45:35 -0700

    adding travis.yml, also fixed correlation test with simulated data

--- testing/automatic/test_correlation.py
+++ testing/automatic/test_correlation.py
@@ -12,7 +12,7 @@ from nose.tools import assert_true, assert_false
 from compare import compare, atlas as Atlas
 from nilearn.image import resample_img
 from template.visual import view
-from scipy.stats import norm
+from scipy.stats import norm, pearsonr
 import testing_functions
 import nibabel
 import random
@@ -21,76 +21,76 @@ import numpy
 import os
 
 def setup_func():
-    thresholds = [0.0,0.5,1.0,1.5,1.96,2.0]
-    mr_directory = os.path.join(os.path.abspath(os.path.dirname(testing_functions.__file__) + "/../.."),"mr") 
+  thresholds = [0.0,0.5,1.0,1.5,1.96,2.0]
+  mr_directory = os.path.join(os.path.abspath(os.path.dirname(testing_functions.__file__) + "/../.."),"mr") 
 
-    # Use 8mm resampled images for speed
-    image1 = "%s/8mm16_zstat1_1.nii" %(mr_directory)
-    image2 = "%s/8mm16_zstat3_1.nii" %(mr_directory)
-    standard = "%s/MNI152_T1_8mm_brain_mask.nii.gz" %(mr_directory)
-    images = [image1,image2]
-    return thresholds,images,standard
+  # Use 8mm resampled images for speed
+  image1 = "%s/8mm16_zstat1_1.nii" %(mr_directory)
+  image2 = "%s/8mm16_zstat3_1.nii" %(mr_directory)
+  standard = "%s/MNI152_T1_8mm_brain_mask.nii.gz" %(mr_directory)
+  images = [image1,image2]
+  return thresholds,images,standard
 
 '''Test pybraincompare correlation method against neurovault method'''
 def test_pbc_vs_neurovault():
 
-    thresholds,images,standard = setup_func()
+  thresholds,images,standard = setup_func()
     
-    # Here we will output scatterplot compare correlations for each threshold
-    df = pandas.DataFrame()
-    nv_corrs = []  # Whole brain correlation calculated with neurovault
-    pbc_corrs = [] # Whole brain correlation calculated with pyraincompare
-    for thresh in thresholds:
-      df_single,nv_corr,pbc_corr = run_scatterplot_compare_correlation(images=images,
+  # Here we will output scatterplot compare correlations for each threshold
+  df = pandas.DataFrame()
+  nv_corrs = []  # Whole brain correlation calculated with neurovault
+  pbc_corrs = [] # Whole brain correlation calculated with pyraincompare
+  for thresh in thresholds:
+    df_single,nv_corr,pbc_corr = run_scatterplot_compare_correlation(images=images,
                                                                        image_names=["image 1","image 2"],
                                                                        threshold=thresh,
                                                                        reference_mask=standard,
                                                                        browser_view=False)
-      nv_corrs.append(nv_corr)
-      pbc_corrs.append(pbc_corr)
-      df = df.append(df_single)
+    nv_corrs.append(nv_corr)
+    pbc_corrs.append(pbc_corr)
+    df = df.append(df_single)
 
-    # We should have the same number
-    assert_equal(len(nv_corrs),len(pbc_corrs))
+  # We should have the same number
+  assert_equal(len(nv_corrs),len(pbc_corrs))
 
-    # And they should almost have equal precision
-    for c in range(0,len(pbc_corrs)):
-      assert_almost_equal(nv_corrs[c],pbc_corrs[c],decimal=4)
+  # And they should almost have equal precision
+  for c in range(0,len(pbc_corrs)):
+    assert_almost_equal(nv_corrs[c],pbc_corrs[c],decimal=4)
 
     
 '''Test that pybraincompare scores are not changing'''
 def test_pbc_correlation_consistent():
 
-    thresholds,images,standard = setup_func()
-    # These are the scores we should get for the thresholds
-    scores = [0.24239542,0.37818813,0.58678013,0.84492999,0.52579623,0.52579623]
+  thresholds,images,standard = setup_func()
+  # These are the scores we should get for the thresholds
+  scores = [0.24239542,0.37818813,0.58678013,0.84492999,0.52579623,0.52579623]
    
-    # Here we will output scatterplot compare correlations for each threshold
-    df = pandas.DataFrame()
-    pbc_corrs = [] # Whole brain correlation calculated with pyraincompare
-    for thresh in thresholds:
-      df_single,nv_corr,pbc_corr = run_scatterplot_compare_correlation(images=images,
+  # Here we will output scatterplot compare correlations for each threshold
+  df = pandas.DataFrame()
+  pbc_corrs = [] # Whole brain correlation calculated with pyraincompare
+  for thresh in thresholds:
+    df_single,nv_corr,pbc_corr = run_scatterplot_compare_correlation(images=images,
                                                                        image_names=["image 1","image 2"],
                                                                        threshold=thresh,
                                                                        reference_mask=standard,
                                                                        browser_view=False)
-      pbc_corrs.append(pbc_corr)
-      df = df.append(df_single)
+    pbc_corrs.append(pbc_corr)
+    df = df.append(df_single)
 
-    # We should have the same number
-    assert_equal(len(scores),len(pbc_corrs))
+  # We should have the same number
+  assert_equal(len(scores),len(pbc_corrs))
 
-    # And they should be equal
-    for c in range(0,len(pbc_corrs)):
-      assert_almost_equal(scores[c],pbc_corrs[c],decimal=4)
+  # And they should be equal
+  for c in range(0,len(pbc_corrs)):
+    assert_almost_equal(scores[c],pbc_corrs[c],decimal=4)
 
-    # Regional correlation scores should also be equal
-    data_directory = os.path.join(os.path.abspath(os.path.dirname(testing_functions.__file__)),"data") 
-    df_standard = pandas.read_csv("%s/regional_scores.tsv" %(data_directory),sep="\t")
-    for row in df_standard.iterrows():
-      subset = df[df.ATLAS_LABELS==row[1].ATLAS_LABELS]
-      subset = subset[subset.threshold==row[1].threshold]
-      assert_almost_equal(subset.ATLAS_CORR.values[0],row[1].ATLAS_CORR,decimal=4)
+  # Regional correlation scores should also be equal
+  data_directory = os.path.join(os.path.abspath(os.path.dirname(testing_functions.__file__)),"data") 
+  df_standard = pandas.read_csv("%s/regional_scores.tsv" %(data_directory),sep="\t")
+  for row in df_standard.iterrows():
+    subset = df[df.ATLAS_LABELS==row[1].ATLAS_LABELS]
+    subset = subset[subset.threshold==row[1].threshold]
+    assert_almost_equal(subset.ATLAS_CORR.values[0],row[1].ATLAS_CORR,decimal=4)
     
 
 # https://github.com/NeuroVault/NeuroVault/issues/133#issuecomment-74464393
@@ -104,23 +104,17 @@ def test_simulated_correlations():
   standard = nibabel.load(standard)
   number_values = len(numpy.where(standard.get_data()!=0)[0])
   numpy.random.seed(9191986)
-  data1 = norm.rvs(size=number_values)
-  #data1 = numpy.random.normal(size=number_values)
   for x in range(0,10):  
+    data1 = norm.rvs(size=number_values)
     data2 = norm.rvs(size=number_values)
-    means = [data1.mean(), data2.mean()]      # should be essentially 0 
-    stds = [data1.std(), data2.std()] # should be essentially 1
-    corr = random.uniform(0,1)        # some correlation
-    covs = [[stds[0]**2         , stds[0]*stds[1]*corr], 
-           [stds[0]*stds[1]*corr,           stds[1]**2]] 
-    m = numpy.random.multivariate_normal(means, covs, number_values).T
+    corr = pearsonr(data1,data2)[0]
       
     # Put into faux nifti images
     mr1 = numpy.zeros(standard.shape)
-    mr1[standard.get_data()!=0] = m[0]
+    mr1[standard.get_data()!=0] = data1
     mr1 = nibabel.nifti1.Nifti1Image(mr1,affine=standard.get_affine(),header=standard.get_header())
     mr2 = numpy.zeros(standard.shape)
-    mr2[standard.get_data()!=0] = m[1]
+    mr2[standard.get_data()!=0] = data2
     mr2 = nibabel.nifti1.Nifti1Image(mr2,affine=standard.get_affine(),header=standard.get_header())  
     score = calculate_pybraincompare_pearson([mr1,mr2])  
-    assert_almost_equal(corr,score,decimal=1)
+    assert_almost_equal(corr,score,decimal=5)
