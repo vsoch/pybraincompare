2a4586f8b0753359a2c35200ae832c036a983558
vsoch <vsochat@stanford.edu>
2015-03-19 06:17:38 -0700

    modified:   .travis.yml 	modified:   pybraincompare/testing/run_tests.sh

--- pybraincompare/testing/run_tests.sh
+++ pybraincompare/testing/run_tests.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-nosetests --verbosity=3 --with-doctest test_correlation.py
+TESTDIR=$1
+TEST_RUN_FOLDER=$2
+
+cd $TEST_RUN_FOLDER
+nosetests --verbosity=3 --with-doctest $TESTDIR/test_correlation.py
