6cd5862f5c170462aaa677ab9f7d4d93999bc3f5
vsoch <vsochat@stanford.edu>
2015-03-19 13:49:41 -0700

    removing neurovault tests and non-simulated data tests

--- pybraincompare/testing/test_correlation.py
+++ pybraincompare/testing/test_correlation.py
@@ -3,11 +3,10 @@
 """
 Test that regional and whole brain correlation scores remain consistent
 """
-from pybraincompare.testing.testing_functions import run_scatterplot_compare_correlation, generate_thresholds, calculate_pybraincompare_pearson
+from pybraincompare.testing.testing_functions import generate_thresholds, calculate_pybraincompare_pearson
 from pybraincompare.mr.datasets import get_pair_images, get_data_directory
 from pybraincompare.compare.mrutils import resample_images_ref, make_binary_deletion_mask, do_mask
 from numpy.testing import assert_array_equal, assert_almost_equal, assert_equal
-from pybraincompare.testing.neurovault_functions import calculate_voxelwise_pearson_similarity
 from pybraincompare.compare.maths import do_pairwise_correlation
 from nose.tools import assert_true, assert_false
 from pybraincompare.compare import compare, atlas as Atlas
@@ -30,67 +29,6 @@ def setup_func():
   standard = "%s/MNI152_T1_2mm_brain_mask.nii.gz" %(mr_directory)
   return thresholds,images,standard
 
-'''Test pybraincompare correlation method against neurovault method'''
-def test_pbc_vs_neurovault():
-
-  thresholds,images,standard = setup_func()
-    
-  # Here we will output scatterplot compare correlations for each threshold
-  df = pandas.DataFrame()
-  nv_corrs = []  # Whole brain correlation calculated with neurovault
-  pbc_corrs = [] # Whole brain correlation calculated with pyraincompare
-  for thresh in thresholds:
-    df_single,nv_corr,pbc_corr = run_scatterplot_compare_correlation(images=images,
-                                                                       image_names=["image 1","image 2"],
-                                                                       threshold=thresh,
-                                                                       reference_mask=standard,
-                                                                       browser_view=False)
-    nv_corrs.append(nv_corr)
-    pbc_corrs.append(pbc_corr)
-    df = df.append(df_single)
-
-  # We should have the same number
-  assert_equal(len(nv_corrs),len(pbc_corrs))
-
-  # And they should almost have equal precision
-  for c in range(0,len(pbc_corrs)):
-    assert_almost_equal(nv_corrs[c],pbc_corrs[c],decimal=4)
-
-    
-'''Test that pybraincompare scores are not changing'''
-def test_pbc_correlation_consistent():
-
-  thresholds,images,standard = setup_func()
-  # These are the scores we should get for the thresholds
-  scores = [0.24239542,0.37818813,0.58678013,0.84492999,0.52579623,0.52579623]
-   
-  # Here we will output scatterplot compare correlations for each threshold
-  df = pandas.DataFrame()
-  pbc_corrs = [] # Whole brain correlation calculated with pyraincompare
-  for thresh in thresholds:
-    df_single,nv_corr,pbc_corr = run_scatterplot_compare_correlation(images=images,
-                                                                       image_names=["image 1","image 2"],
-                                                                       threshold=thresh,
-                                                                       reference_mask=standard,
-                                                                       browser_view=False)
-    pbc_corrs.append(pbc_corr)
-    df = df.append(df_single)
-
-  # We should have the same number
-  assert_equal(len(scores),len(pbc_corrs))
-
-  # And they should be equal
-  for c in range(0,len(pbc_corrs)):
-    assert_almost_equal(scores[c],pbc_corrs[c],decimal=4)
-
-  # Regional correlation scores should also be equal
-  data_directory = os.path.join(os.path.abspath(os.path.dirname(testing_functions.__file__)),"data") 
-  df_standard = pandas.read_csv("%s/regional_scores.tsv" %(data_directory),sep="\t")
-  for row in df_standard.iterrows():
-    subset = df[df.ATLAS_LABELS==row[1].ATLAS_LABELS]
-    subset = subset[subset.threshold==row[1].threshold]
-    assert_almost_equal(subset.ATLAS_CORR.values[0],row[1].ATLAS_CORR,decimal=4)
-    
 
 # https://github.com/NeuroVault/NeuroVault/issues/133#issuecomment-74464393
 '''Test that pybraincompare returns same simulated correlations'''
--- pybraincompare/testing/testing_functions.py
+++ pybraincompare/testing/testing_functions.py
@@ -1,5 +1,4 @@
 from pybraincompare.compare.mrutils import resample_images_ref, make_binary_deletion_mask, do_mask
-from pybraincompare.testing.neurovault_functions import calculate_voxelwise_pearson_similarity
 from pybraincompare.compare.maths import do_pairwise_correlation
 from pybraincompare.compare import compare, atlas as Atlas
 from nilearn.image import resample_img
@@ -15,52 +14,6 @@ def generate_thresholds():
     thresholds = thresholds + [(float(x) * 0.01)+ii for x in range(0,100)]
   return thresholds
 
-# Test interface output with images thresholded at particular value
-def run_scatterplot_compare_threshold(images,image_names,threshold,atlas=None,reference_mask=None,browser_view=False):
-  # Threshold the images
-  thresholded_images = []
-  for image in images:
-    mr = nibabel.load(image)
-    thresholded_images.append(threshold_abs(mr,threshold))
-
-  # Report overlap of voxels
-  pdmask = make_binary_deletion_mask(thresholded_images)
-  overlapping_voxels = len(numpy.where(pdmask==1)[0])
-  print "%s overlapping voxels at threshold %s" %(overlapping_voxels,threshold)
-
-  # Generate the html_snippet
-  if reference_mask==None:
-    html_snippet,data_table = compare.scatterplot_compare(images=thresholded_images,image_names=image_names,atlas=atlas,software="FSL",corr="pearson") 
-  else: 
-    html_snippet,data_table = compare.scatterplot_compare(images=thresholded_images,image_names=image_names,atlas=atlas,software="FSL",corr="pearson",reference_mask=reference_mask) 
-
-  # If we want to view in the browser
-  if browser_view == True:
-    view(html_snippet)
-
-  return html_snippet,data_table,thresholded_images, overlapping_voxels
-
-
-# Test scatterplot compare correlations against Neurovault with different thresholds
-def run_scatterplot_compare_correlation(images,image_names,threshold,atlas=None,reference_mask=None,browser_view=False):
-  
-  # Get the html snippet and data table (with regional correlations)
-  html_snippet,data_table,thresholded_images,overlap = run_scatterplot_compare_threshold(images=images,image_names=image_names,threshold=threshold,atlas=atlas,reference_mask=reference_mask,browser_view=browser_view)
-  
-  # Format the regional correlations into a smaller table
-  scores = data_table.loc[:,["ATLAS_LABELS","ATLAS_CORR"]]
-  scores = scores.drop_duplicates()
-  df = pandas.DataFrame(scores)
-  df["threshold"] = threshold
-  df["overlapping_voxels"] = overlap
-
-  # Calculate a whole brain correlation using same NeuroVault function
-  nv_correlation = calculate_voxelwise_pearson_similarity(thresholded_images[0], thresholded_images[1])
-
-  # Calculate a whole brain correlation with same procedure as in pybraincompare
-  pbc_correlation = calculate_pybraincompare_pearson(thresholded_images)
-  return df,nv_correlation,pbc_correlation
-
 # Calculate a whole brain correlation with same procedure as in pybraincompare
 def calculate_pybraincompare_pearson(images):
   pdmask = make_binary_deletion_mask(images)
@@ -69,13 +22,6 @@ def calculate_pybraincompare_pearson(images):
   masked = pandas.DataFrame(numpy.transpose(masked))
   pbc_correlation = do_pairwise_correlation(masked[0],masked[1],corr_type="pearson")
   return pbc_correlation["No Label"]
-
-# General running function for some set of images and an atlas
-def run_scatterplot_compare(images,image_names,atlas,atlas_rendering=None,reference_mask=None):
-  if reference_mask == None:
-    html_snippet,data_table = compare.scatterplot_compare(images=images,image_names=image_names,atlas=atlas,atlas_rendering=atlas_rendering,software="FSL",corr="pearson") 
-  else:
-    html_snippet,data_table = compare.scatterplot_compare(images=images,image_names=image_names,atlas=atlas,atlas_rendering=atlas_rendering,software="FSL",corr="pearson",reference_mask=reference_mask)  
 
 # Threshold the image
 def threshold_abs(image1,thresh):
