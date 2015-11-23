77650ff66810948f704809047eada1e1114500e4
vsoch <vsochat@stanford.edu>
2015-11-23 12:38:15 -0800

    modified:   example/make_ontology_tree.py

--- example/make_ontology_tree.py
+++ example/make_ontology_tree.py
@@ -8,7 +8,7 @@ relationship_table = "data/cogatlas_annotate_triples.tsv"
 
 # Create a data structure for d3 visualization and data analysis (no output json, specify with output_json=)
 data_structure = named_ontology_tree_from_tsv(relationship_table)
-html_snippet = ontology.make_ontology_tree_d3(data_structure)
+html_snippet = make_ontology_tree_d3(data_structure)
 
 # View in browser!
 view(html_snippet)
