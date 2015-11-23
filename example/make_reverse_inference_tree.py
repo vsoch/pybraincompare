d0aed3ae99a4f42043dc0f2954236e51b742d9b6
vsoch <vsochat@stanford.edu>
2015-11-23 12:37:25 -0800

    updating examples

--- example/make_reverse_inference_tree.py
+++ example/make_reverse_inference_tree.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 
 import pandas as pd
-from pybraincompare.annotate import ontology
+from pybraincompare.ontology.tree import named_ontology_tree_from_tsv, make_ontology_tree_d3
 from pybraincompare.template.visual import view
 
 # This defines a "mock" ontology for the openfmri proof of concept set - names should be unique
@@ -11,12 +11,11 @@ relationship_table = "data/cogatlas_annotate_triples.tsv"
 reverse_inference_df = pd.read_csv("data/reverse_inference_scores.tsv",sep="\t")
 reverse_inferences = dict()
 for row in reverse_inference_df.iterrows():
-  reverse_inferences[row[1].NODE_GROUP] = row[1].REVERSE_INFERENCE_SCORE
+    reverse_inferences[row[1].NODE_GROUP] = row[1].REVERSE_INFERENCE_SCORE
 
 # Create a data structure for d3 visualization and data analysis (no output json, specify with output_json=)
-data_structure = ontology.named_ontology_tree_from_tsv(relationship_table, meta_data = reverse_inferences)
-
-html_snippet = ontology.make_reverse_inference_tree_d3(data_structure)
+data_structure = named_ontology_tree_from_tsv(relationship_table, meta_data = reverse_inferences)
+html_snippet = make_reverse_inference_tree_d3(data_structure)
 
 # View in browser!
 view(html_snippet)
